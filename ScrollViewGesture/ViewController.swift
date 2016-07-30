b1b737700a5a07d098823d050932676c9aa42f81
Harry Ng <harryworld@gmail.com>
2016-07-31 02:13:33 +0800

    Initial Commit

---
+++ ScrollViewGesture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScrollViewGesture
+//
+//  Created by Harry Ng on 31/7/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
